34ffa8f0837e13d50803d89263f7fc8955899795
EPPI-c <109323250+EPPI-c@users.noreply.github.com>
2023-03-12 20:02:31 -0300

    rank separated per channel

--- rank.py
+++ rank.py
@@ -7,40 +7,44 @@ onetoten = ('1️⃣','2️⃣','3️⃣','4️⃣','5️⃣','6️⃣','7️⃣
 class Rank(commands.Cog):
     def __init__(self, bot:commands.Bot) -> None:
         self.bot = bot
-        self.ranking = []
+        self.ranking = {}
 
     @commands.command()
     async def his(self, ctx, amount:int):
         async with ctx.typing():
             messages = ctx.channel.history(limit=amount)
-            self.ranking = [m async for m in messages if m.content.startswith('>>')]
+            self.ranking[ctx.channel.id] = [m async for m in messages if m.content.startswith('>>')]
 
     @commands.command(name='>')
     async def entry(self, ctx):
         async with ctx.typing():
-            self.ranking.append(ctx.message)
+            if channel := self.ranking.get(ctx.channel.id):
+                channel.append(ctx.message)
+            else:
+                self.ranking[ctx.channel.id] = [ctx.message]
             for e in onetoten:
                 await ctx.message.add_reaction(e)
 
     @commands.command()
     async def show(self, ctx):
         async with ctx.typing():
+            if not (channel := self.ranking.get(ctx.channel.id)):
+                return
             global ranking
             ra = []
             start = time.time()
-            async with ctx.typing():
-                for msg in self.ranking:
-                    await self.retrieve_reactions(ra, msg)
-                ra.sort(key=lambda x: x['mean'], reverse=True)
-                for i, a in enumerate(ra, start=1):
-                    await a['msg'].channel.send(f'{i} - {a["msg"].content.removeprefix(">>")}: {a["mean"]}')
+            for msg in channel:
+                await self.retrieve_reactions(ra, msg)
+            ra.sort(key=lambda x: x['mean'], reverse=True)
+            for i, a in enumerate(ra, start=1):
+                await a['msg'].channel.send(f'{i} - {a["msg"].content.removeprefix(">>")}: {a["mean"]}')
             end = time.time()
             print(end-start)
 
     @commands.command()
     async def clean(self, ctx):
         async with ctx.typing():
-            self.ranking = []
+            self.ranking[ctx.channel.id] = []
 
     async def retrieve_reactions(self, ra, msg):
         mean = 0
